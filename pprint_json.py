3ec1f8083fd622eb3c5c3a73f1385b25303f2632
DanGalkin <dangalkin@gmail.com>
2017-01-15 23:54:07 +0300

    Update pprint_json.py
    Written the whole program from scratch

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,15 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as json_data_file:
+        json_data = json.loads(json_data_file.read())
+    return json_data
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    json_filepath = input('Где лежит JSON-файл, который приводим в порядок?')
+    pretty_print_json(load_data(json_filepath)[0])
